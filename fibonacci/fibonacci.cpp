42fc600449d150521a432bfd93f8881021f37d64
tushuntir <tushuntirilgan@gmail.com>
2026-07-02 23:39:52 +0500

    Organize files into separate folders by name

---
+++ fibonacci/fibonacci.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string sentence;
+    cout << "Enter something: ";
+    getline(cin, sentence);
+    for (char letter : sentence) {
+        int count;
+        letter = 
+        letter += count;
+    }
+}
---
+++ prime/prime.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+
+bool isPrime(int a) {
+    if (a <= 1) {
+        return false;
+    } else if (a % a == 0) {
+        return true;
+    }
+}
+int main() {
+    
+}
